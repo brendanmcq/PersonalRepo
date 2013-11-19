c7ae169f6dd5e5549774ee0ceff497ff8c0cff6b
brendan <brendan.ftw@gmail.com>
2013-11-19 14:41:42 -0400

    commit 2

---
+++ PersonalRepo/CS4999/src/com/fighterfinder/crawler/FightCrawler.java
@@ -0,0 +1,13 @@
+package com.fighterfinder.crawler;
+
+public class FightCrawler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
