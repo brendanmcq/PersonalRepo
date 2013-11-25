6e7d0f289ce154214d0f5cd044e4ad43a556ca07
brendan <brendan.ftw@gmail.com>
2013-11-25 16:16:04 -0400

    Grabbing attr values in TestMain

--- PersonalRepo/CS4999/src/com/fighterfinder/test/TestMain.java
+++ PersonalRepo/CS4999/src/com/fighterfinder/test/TestMain.java
@@ -1,6 +1,8 @@
 package com.fighterfinder.test;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import com.fighterfinder.crawler.FightCrawler;
 
@@ -9,9 +11,17 @@ public class TestMain {
 	/**
 	 * @param args
 	 */
+	//http://jsoup.org/apidocs/
 	public static void main(String[] args) {
 			FightCrawler crawler = new FightCrawler();
 			String url = "http://www.sherdog.com/events";
 			Document doc = crawler.getHtmlDoc(url);
+			Elements elements = doc.getElementById("events_list").getElementsByTag("meta");
+			for(Element element : elements){
+			System.out.println(element);
+			System.out.println("attr: " + element.attr("content"));
+			System.out.println("***");
+			}
+			
 	}
 }
